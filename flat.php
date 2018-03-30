74b35d008862bc37ae2f529d5e9fa8b21e4a1e8d
Ivan <vania149895@gmail.com>
2018-03-30 21:46:11 +0300

    Создан пользовательский интерфейс
    Разработан CSS GRID, nav-menu.
    Готова стартавая страница на языке PHP. Множество различных улучшений и дополнений.
    JS, JQ и Botstrap пока не использовался.
    Первая релизная версия ver. 0.1

---
+++ flat.php
@@ -0,0 +1,100 @@
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru-ru" lang="ru-ru" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">-->
+    <link rel="stylesheet" type="text/css" href="css/main.css" >
+    <style>* {margin: 0;padding: 0;} </style> <!--Сброс отступов всего документа-->
+<body class="container">
+
+<header class="element element-1">
+    <div><a href="index.php">PROJECT GEN</a> <p>(ver. 0.1)</p></div>
+</header>
+
+<nav class="element element-2">
+
+    <ul class="nav-menu">
+        <li><a href="flat.php" class="selected">Квартира</a></li>
+        <li><a href="#">Дом</a></li>
+        <li><a href="#">Дача</a></li>
+        <li><a href="#">Участок</a></li>
+    </ul>
+</nav>
+
+<main class="element element-3">
+    <div class="left">Инст</div>
+    <div class="work">Интерфейс</div>
+    <div class="right">Доп</div>
+</main>
+
+<footer class="element element-4">
+    Project GEN by Ivan Borusiuk 2018
+</footer>
+
+<!--
+<div class="grid">
+    <div class="grid-item grid-item1">Item 1</div>
+    <div class="grid-item grid-item2">Item 2</div>
+    <div class="grid-item grid-item3">Item 3</div>
+</div>
+
+
+
+
+
+
+<nav>
+    <ul>
+        <li><a href="index_1.php">Project GEN (ver. 0.1)</a></li>
+        <li><a href="admin.php">ADMIN</a></li>
+    </ul>
+</nav>
+
+<nav aria-label="breadcrumb">
+    <p> Project GEN (ver.0.1)</p>
+
+    <ul class="breadcrumb">
+        <h6>Генератор текстов Ver. 0.1</h6>
+        <li class="breadcrumb-item active" aria-current="page">Главная</li>
+        <li>ВХОД ДЛЯ АДМИНА</li>
+
+    </ul>
+</nav>
+
+
+<div class="col-4">
+    <div class="list-group" id="list-tab" role="tablist">
+        <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Общие характеристики</a>
+        <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Доп. по квартире</a>
+        <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Доп по дому</a>
+        <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Креатив</a>
+    </div>
+</div>
+
+
+<div class="container">
+    <div class="jumbotron">
+        <h1 class="display-4">Привет мой друг!</h1>
+        <p class="lead">Это программа была созданна специалльно для тебя, что бы ты смог создать прекрастное описания для своей недвижимости.</p>
+    </div>
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link active" href="flat.html">Квартиры</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="#">Дома</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="#">Дачи</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="#">Участки</a>
+        </li>
+    </ul>
+</div>
+<footer>Project GEN by Ivan Borusiuk 2018</footer>-->
+</body>
+</head>
